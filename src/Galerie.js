f64cfcd74db4295c548fe5fe5fba64a0c7b65b4b
Mélissa Morin <melissa.morinmangaillou@gmail.com>
2019-08-13 21:05:25 -0400

    Etape 1

--- src/Galerie.js
+++ src/Galerie.js
@@ -7,8 +7,42 @@ export default class Galerie {
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		var backdrop = document.createElement("div");
+		backdrop.id = "backdrop";
+
+		var close = document.createElement("span");
+		close.classList.add("close");
+		close.innerHTML = "&#x2716;";
+
+		var precedent = document.createElement("span");
+		precedent.classList.add("precedent");
+		precedent.innerHTML = "&#x276e;";
+
+		var diapo = document.createElement("figure");
+		diapo.classList.add("diapo");
+
+		var img = document.createElement("img");
+		img.src = "images/niche.jpg";
+		img.alt = "Chien dans sa niche";
+
+		var figcaption = document.createElement("figcaption");
+		figcaption.innerHTML = "Chien dans sa niche";
+
+		var suivant = document.createElement("span");
+		suivant.classList.add("suivant");
+		suivant.innerHTML = "&#x276f;";
+
+		var body = document.body;
+
+		diapo.appendChild(img);
+		diapo.appendChild(figcaption);
+
+		backdrop.appendChild(close);
+		backdrop.appendChild(precedent);
+		backdrop.appendChild(diapo);
+		backdrop.appendChild(suivant);
+
+		body.appendChild(backdrop);
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
